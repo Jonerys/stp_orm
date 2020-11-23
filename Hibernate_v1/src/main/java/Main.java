817620d1eaf4e1bc36dd6f81463bae50e4dd3a9c
Jonerys <pavel-0199@mail.ru>
2020-11-23 10:14:01 +0500

    добавил через Criteria, библиотеки мне накачал add framework support, я их снес, gitignore поправил

--- Hibernate_v1/src/main/java/Main.java
+++ Hibernate_v1/src/main/java/Main.java
@@ -9,6 +9,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.criteria.*;
 import javax.persistence.metamodel.EntityType;
 import javax.swing.*;
 
@@ -54,7 +55,12 @@ public class Main extends JFrame {
                 switch (ch) {
                     case '0':
                         try (Session session = DataAccess.createSessionFactory().openSession()) {
-                            Query query = session.createQuery("FROM GoodsMainEntity ORDER BY id asc");
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaQuery<GoodsMainEntity> criteriaSelect = criteriaBuilder.createQuery(GoodsMainEntity.class);
+                            Root<GoodsMainEntity> root = criteriaSelect.from(GoodsMainEntity.class);
+                            criteriaSelect.orderBy(criteriaBuilder.asc(root.get("id")));
+                            criteriaSelect.select(root);
+                            Query query = session.createQuery(criteriaSelect);
                             List<GoodsMainEntity> goods = query.list();
                             System.out.println("goods_main\n\nid\tname");
                             for (GoodsMainEntity s : goods) {
@@ -90,10 +96,13 @@ public class Main extends JFrame {
                             System.out.print("\nВведите новое название: ");
                             Scanner sc2 = new Scanner(System.in);
                             String str = sc2.nextLine();
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaUpdate<GoodsMainEntity> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(GoodsMainEntity.class);
+                            Root<GoodsMainEntity> root = criteriaUpdate.from(GoodsMainEntity.class);
+                            criteriaUpdate.set("name", str);
+                            criteriaUpdate.where(criteriaBuilder.equal(root.get("id"), id));
                             transaction = session.beginTransaction();
-                            Query query = session.createQuery("UPDATE GoodsMainEntity SET name = :name WHERE id = :o_id");
-                            query.setParameter("o_id", id);
-                            query.setParameter("name", str);
+                            Query query = session.createQuery(criteriaUpdate);
                             int result = query.executeUpdate();
                             transaction.commit();
                             System.out.println("Обновлено строк: " + result + "\n\n");
@@ -108,9 +117,12 @@ public class Main extends JFrame {
                             Scanner sc = new Scanner(System.in);
                             int id = sc.nextInt();
                             if (id < 0) break;
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaDelete<GoodsMainEntity> criteriaDelete = criteriaBuilder.createCriteriaDelete(GoodsMainEntity.class);
+                            Root<GoodsMainEntity> root = criteriaDelete.from(GoodsMainEntity.class);
+                            criteriaDelete.where(criteriaBuilder.equal(root.get("id"), id));
                             transaction = session.beginTransaction();
-                            Query query = session.createQuery("DELETE FROM GoodsMainEntity WHERE id = :o_id");
-                            query.setParameter("o_id", id);
+                            Query query = session.createQuery(criteriaDelete);
                             int result = query.executeUpdate();
                             transaction.commit();
                             System.out.println("Удалено строк: " + result + "\n\n");
@@ -121,7 +133,12 @@ public class Main extends JFrame {
                         break;
                     case '4':
                         try (Session session = DataAccess.createSessionFactory().openSession()) {
-                            Query query = session.createQuery("FROM GoodswhEntity ORDER BY id asc");
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaQuery<GoodswhEntity> criteriaSelect = criteriaBuilder.createQuery(GoodswhEntity.class);
+                            Root<GoodswhEntity> root = criteriaSelect.from(GoodswhEntity.class);
+                            criteriaSelect.orderBy(criteriaBuilder.asc(root.get("id")));
+                            criteriaSelect.select(root);
+                            Query query = session.createQuery(criteriaSelect);
                             List<GoodswhEntity> gwh = query.list();
                             System.out.println("goodswh\n\nid\tg_name | w_name");
                             for (GoodswhEntity s : gwh) {
@@ -138,15 +155,23 @@ public class Main extends JFrame {
                             Scanner sc = new Scanner(System.in);
                             int gId = sc.nextInt();
                             if (gId < 0) break;
-                            Query query = session.createQuery("FROM GoodsMainEntity WHERE id = :g_id");
-                            query.setParameter("g_id", gId);
-                            List<GoodsMainEntity> goods = query.list();
+
                             System.out.print("\nВведите ИД склада (для выхода введите отрицательный ИД): ");
                             Scanner sc2 = new Scanner(System.in);
                             int wId = sc2.nextInt();
                             if (wId < 0) break;
-                            Query query2 = session.createQuery("FROM WarehousesEntity WHERE id = :w_id");
-                            query2.setParameter("w_id", wId);
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaQuery<GoodsMainEntity> criteriaSelect1 = criteriaBuilder.createQuery(GoodsMainEntity.class);
+                            Root<GoodsMainEntity> root1 = criteriaSelect1.from(GoodsMainEntity.class);
+                            criteriaSelect1.where(criteriaBuilder.equal(root1.get("id"), gId));
+                            criteriaSelect1.select(root1);
+                            Query query1 = session.createQuery(criteriaSelect1);
+                            List<GoodsMainEntity> goods = query1.list();
+                            CriteriaQuery<WarehousesEntity> criteriaSelect2 = criteriaBuilder.createQuery(WarehousesEntity.class);
+                            Root<WarehousesEntity> root2 = criteriaSelect2.from(WarehousesEntity.class);
+                            criteriaSelect2.where(criteriaBuilder.equal(root2.get("id"), wId));
+                            criteriaSelect2.select(root2);
+                            Query query2 = session.createQuery(criteriaSelect2);
                             List<WarehousesEntity> warehouses = query2.list();
                             GoodswhEntity gwh = new GoodswhEntity();
                             gwh.setGoodsMainByIdGd(goods.get(0));
@@ -172,33 +197,49 @@ public class Main extends JFrame {
                             System.out.print("\nВведите новый ИД склада (если хотите оставить введите отрицательный ИД): ");
                             Scanner sc3 = new Scanner(System.in);
                             int wId = sc3.nextInt();
-                            Query query = session.createQuery("FROM GoodsMainEntity WHERE id = :g_id");
-                            query.setParameter("g_id", gId);
-                            List<GoodsMainEntity> ng = query.list();
-                            Query query2 = session.createQuery("FROM WarehousesEntity WHERE id = :w_id");
-                            query2.setParameter("w_id", wId);
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaQuery<GoodsMainEntity> criteriaSelect1 = criteriaBuilder.createQuery(GoodsMainEntity.class);
+                            Root<GoodsMainEntity> root1 = criteriaSelect1.from(GoodsMainEntity.class);
+                            criteriaSelect1.where(criteriaBuilder.equal(root1.get("id"), gId));
+                            criteriaSelect1.select(root1);
+                            Query query1 = session.createQuery(criteriaSelect1);
+                            List<GoodsMainEntity> ng = query1.list();
+                            CriteriaQuery<WarehousesEntity> criteriaSelect2 = criteriaBuilder.createQuery(WarehousesEntity.class);
+                            Root<WarehousesEntity> root2 = criteriaSelect2.from(WarehousesEntity.class);
+                            criteriaSelect2.where(criteriaBuilder.equal(root2.get("id"), wId));
+                            criteriaSelect2.select(root2);
+                            Query query2 = session.createQuery(criteriaSelect2);
                             List<WarehousesEntity> nw = query2.list();
                             Query query3;
                             if (gId < 0 && wId < 0) {
                                 System.out.println("Обновлено строк: 0\n\n");
                                 break;
                             } else if (gId < 0) {
-                                query3 = session.createQuery("UPDATE GoodswhEntity SET warehousesByIdWh = :nw WHERE id = :o_id");
-                                query3.setParameter("nw", nw.get(0));
+                                CriteriaUpdate<GoodswhEntity> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(GoodswhEntity.class);
+                                Root<GoodswhEntity> root = criteriaUpdate.from(GoodswhEntity.class);
+                                criteriaUpdate.set("warehousesByIdWh", nw.get(0));
+                                criteriaUpdate.where(criteriaBuilder.equal(root.get("id"), id));
+                                query3 = session.createQuery(criteriaUpdate);
                             } else if (wId < 0) {
-                                query3 = session.createQuery("UPDATE GoodswhEntity SET goodsMainByIdGd = :ng WHERE id = :o_id");
-                                query3.setParameter("ng", ng.get(0));
+                                CriteriaUpdate<GoodswhEntity> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(GoodswhEntity.class);
+                                Root<GoodswhEntity> root = criteriaUpdate.from(GoodswhEntity.class);
+                                criteriaUpdate.set("goodsMainByIdGd", ng.get(0));
+                                criteriaUpdate.where(criteriaBuilder.equal(root.get("id"), id));
+                                query3 = session.createQuery(criteriaUpdate);
                             } else {
-                                query3 = session.createQuery( "UPDATE GoodswhEntity SET goodsMainByIdGd = :ng, warehousesByIdWh = :nw WHERE id = :o_id");
-                                query3.setParameter("ng", ng.get(0));
-                                query3.setParameter("nw", nw.get(0));
+                                CriteriaUpdate<GoodswhEntity> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(GoodswhEntity.class);
+                                Root<GoodswhEntity> root = criteriaUpdate.from(GoodswhEntity.class);
+                                criteriaUpdate.set("goodsMainByIdGd", ng.get(0));
+                                criteriaUpdate.set("warehousesByIdWh", nw.get(0));
+                                criteriaUpdate.where(criteriaBuilder.equal(root.get("id"), id));
+                                query3 = session.createQuery(criteriaUpdate);
                             }
-                            query3.setParameter("o_id", id);
 
                             transaction = session.beginTransaction();
                             int result = query3.executeUpdate();
                             transaction.commit();
                             System.out.println("Обновлено строк: " + result + "\n\n");
+
                         } catch (Exception e) {
                             System.out.println("Ошибка UPDATE\nПодробнее: " + e.getMessage() + "\n\n");
                             transaction.rollback();
@@ -210,9 +251,12 @@ public class Main extends JFrame {
                             Scanner sc = new Scanner(System.in);
                             int id = sc.nextInt();
                             if (id < 0) break;
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaDelete<GoodswhEntity> criteriaDelete = criteriaBuilder.createCriteriaDelete(GoodswhEntity.class);
+                            Root<GoodswhEntity> root = criteriaDelete.from(GoodswhEntity.class);
+                            criteriaDelete.where(criteriaBuilder.equal(root.get("id"), id));
                             transaction = session.beginTransaction();
-                            Query query = session.createQuery("DELETE FROM GoodswhEntity WHERE id = :o_id");
-                            query.setParameter("o_id", id);
+                            Query query = session.createQuery(criteriaDelete);
                             int result = query.executeUpdate();
                             transaction.commit();
                             System.out.println("Удалено строк: " + result + "\n\n");
@@ -223,7 +267,12 @@ public class Main extends JFrame {
                         break;
                     case '8':
                         try (Session session = DataAccess.createSessionFactory().openSession()) {
-                            Query query = session.createQuery("FROM WarehousesEntity ORDER BY id asc");
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaQuery<WarehousesEntity> criteriaSelect = criteriaBuilder.createQuery(WarehousesEntity.class);
+                            Root<WarehousesEntity> root = criteriaSelect.from(WarehousesEntity.class);
+                            criteriaSelect.orderBy(criteriaBuilder.asc(root.get("id")));
+                            criteriaSelect.select(root);
+                            Query query = session.createQuery(criteriaSelect);
                             List<WarehousesEntity> warehouses = query.list();
                             System.out.println("warehouses\n\nid\tname");
                             for (WarehousesEntity s : warehouses) {
@@ -277,9 +326,12 @@ public class Main extends JFrame {
                             Scanner sc = new Scanner(System.in);
                             int id = sc.nextInt();
                             if (id < 0) break;
+                            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+                            CriteriaDelete<WarehousesEntity> criteriaDelete = criteriaBuilder.createCriteriaDelete(WarehousesEntity.class);
+                            Root<WarehousesEntity> root = criteriaDelete.from(WarehousesEntity.class);
+                            criteriaDelete.where(criteriaBuilder.equal(root.get("id"), id));
                             transaction = session.beginTransaction();
-                            Query query = session.createQuery("DELETE FROM WarehousesEntity WHERE id = :o_id");
-                            query.setParameter("o_id", id);
+                            Query query = session.createQuery(criteriaDelete);
                             int result = query.executeUpdate();
                             transaction.commit();
                             System.out.println("Удалено строк: " + result + "\n\n");
